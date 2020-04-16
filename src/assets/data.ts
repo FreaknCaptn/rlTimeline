8b97cb1eebe23dbd7a4242a27025e4bdee5cb3fe
FreaknDeku <cruzjone@gmail.com>
2020-04-15 19:36:51 -0700

    Added static data

---
+++ src/assets/data.ts
@@ -0,0 +1,75 @@
+export const rocketLegueEvents: TimelineEvent[] = [
+    {
+        Header: 'RLCS Season 1: iBuyPower',
+        HeaderContent: 'Kronovi, Lachino and Overzero',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 2: FlipSid3 Tactics',
+        HeaderContent: 'Kuxir, Markydooda and Greazy',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 3: Northern Gaming',
+        HeaderContent: 'Remkoe, Deevo and Turbopolsa',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 4: Gale Force',
+        HeaderContent: 'Violentpanda, Kaydop and Turbopolsa',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 1: Dignitas',
+        HeaderContent: 'Violentpanda, Kaydop and Turbopolsa',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 6: Cloud9',
+        HeaderContent: 'Squishy, Torment and Gimmick',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 7: Renault Vitality',
+        HeaderContent: 'Kaydop, Fairypeak and Scrub Killa',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 8: NRG',
+        HeaderContent: 'GarretG, JSTN and Turbopolsa',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    },
+    {
+        Header: 'RLCS Season 9: CANCELLED',
+        HeaderContent: 'Coronavirus',
+        Content: 'More Info',
+        SideImage: 'rlcsSeason03.jpg',
+        Date: new Date('2016-08-07'),
+    }
+]
+
+export interface TimelineEvent {
+    Header: string;
+    HeaderContent: string;
+    Content: string;
+    SideImage: string;
+    Date: Date
+}
+
+
