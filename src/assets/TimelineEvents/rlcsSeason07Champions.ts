bb8967551a04e1d6e1bc75eab9933767866b7e67
FreaknDeku <cruzjone@gmail.com>
2020-04-17 22:50:31 -0700

    updated data

--- src/assets/TimelineEvents/rlcsSeason07Champions.ts
+++ src/assets/TimelineEvents/rlcsSeason07Champions.ts
@@ -1,10 +1,12 @@
-import { TimelineEvent } from '../timelineEvents';
+import { TimelineEvent, ReferenceLink } from '../timelineEvents';
 
 export const rlcsSeason07Champions: TimelineEvent = {
     Header: 'RLCS Season 7: Renault Vitality',
     HeaderContent: 'Kaydop, Fairypeak and Scrub Killa',
     SideImage: 'temp.jpg',
     Date: new Date('2019-06-23'),
-    Videos: [],
+    Videos: [
+        new ReferenceLink('Season 7 World Championship Aftermovie', 'https://www.youtube.com/embed/LCvU5nfKy8o')
+    ],
     Links: [],
 }
