162ef9adf183ebc2937065e1cc2893dce998d37d
FreaknDeku <cruzjone@gmail.com>
2020-04-24 12:35:53 -0700

    remove ref

--- src/app/timeline/timeline.component.spec.ts
+++ src/app/timeline/timeline.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimelineComponent } from './timeline.component';
 import { DeviceDetectorService } from 'ngx-device-detector';
