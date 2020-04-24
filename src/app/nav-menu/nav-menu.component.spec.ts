5d6be070d235685e7e3141eb52a672c7ad543fe2
FreaknDeku <cruzjone@gmail.com>
2020-04-24 12:34:33 -0700

    update test file format

--- src/app/nav-menu/nav-menu.component.spec.ts
+++ src/app/nav-menu/nav-menu.component.spec.ts
@@ -3,23 +3,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavMenuComponent } from './nav-menu.component';
 
 describe('NavMenuComponent', () => {
-  let component: NavMenuComponent;
+  let navMenuComponent: NavMenuComponent;
   let fixture: ComponentFixture<NavMenuComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NavMenuComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [NavMenuComponent]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavMenuComponent);
-    component = fixture.componentInstance;
+    navMenuComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initialize navmenu component', () => {
+    expect(navMenuComponent).toBeTruthy();
   });
 });
