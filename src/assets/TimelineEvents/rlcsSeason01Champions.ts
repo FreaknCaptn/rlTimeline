fa0d405dd142afea963a3d1a69160df403d5fa31
FreaknDeku <cruzjone@gmail.com>
2020-05-01 18:40:39 -0700

    Removed side content when desktop; updated data

--- src/assets/TimelineEvents/rlcsSeason01Champions.ts
+++ src/assets/TimelineEvents/rlcsSeason01Champions.ts
@@ -1,10 +1,14 @@
-import { TimelineEvent } from '../timelineEvents';
+import { TimelineEvent, ReferenceLink } from '../timelineEvents';
 
 export const rlcsSeason01Champions: TimelineEvent = {
-    Header: 'iBuyPower',
-    HeaderContent: 'aaaKronovi, Lachino and Overzero',
-    SideImage: 'temp.jpg',
+    Header: 'RLCS Season 1 World Championship',
+    HeaderContent: 'Kronovi, Lachinio and OverZero',
+    SideImage: 'rlcsSeason01.png',
     Date: new Date('2016-08-07'),
-    Videos: [],
-    Links: [],
+    Videos: [
+        new ReferenceLink('RLCS Season 1 Finals', 'https://www.youtube.com/embed/9yC6HD39sZQ')
+    ],
+    Links: [
+        new ReferenceLink('RLCS Season 1 Liquipedia Page', 'https://liquipedia.net/rocketleague/Rocket_League_Championship_Series/Season_1')
+    ],
 }
--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,24 +1,8 @@
 import { TimelineEvent } from './timelineEvents';
-import { rlcsSeason09Cancelled } from './TimelineEvents/rlcsSeason09Cancelled';
 import {rlcsSeason01Champions } from './TimelineEvents/rlcsSeason01Champions'
-import { rlcsSeason03Champions } from './TimelineEvents/rlcsSeason03Champions';
-import { rlcsSeason04Champions } from './TimelineEvents/rlcsSeason04Champions';
-import { rlcsSeason05Champions } from './TimelineEvents/rlcsSeason05Champions';
-import { rlcsSeason06Champions } from './TimelineEvents/rlcsSeason06Champions';
-import { rlcsSeason07Champions } from './TimelineEvents/rlcsSeason07Champions';
-import { rlcsSeason08Champions } from './TimelineEvents/rlcsSeason08Champions';
-import { rlcsSeason02Champions } from './TimelineEvents/rlcsSeason02Champions';
 
 export const rocketLegueEvents: TimelineEvent[] = [
-    rlcsSeason01Champions,
-    rlcsSeason02Champions,
-    rlcsSeason03Champions,
-    rlcsSeason04Champions,
-    rlcsSeason05Champions,
-    rlcsSeason06Champions,
-    rlcsSeason07Champions,
-    rlcsSeason08Champions,
-    rlcsSeason09Cancelled,
+    rlcsSeason01Champions
 ]
 
 
