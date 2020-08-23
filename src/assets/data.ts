fe034c9600b2201560b9f383f905b76626e07f58
cruzjone <cruzjone@gmail.com>
2020-08-23 16:10:08 -0700

    add rlcs season 2

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,8 +1,10 @@
 import { TimelineEvent } from './timelineEvents';
 import {rlcsSeason01Champions } from './TimelineEvents/rlcsSeason01Champions'
+import { rlcsSeason02Champions } from './TimelineEvents/rlcsSeason02Champions';
 
 export const rocketLegueEvents: TimelineEvent[] = [
-    rlcsSeason01Champions
+    rlcsSeason01Champions,
+    rlcsSeason02Champions
 ]
 
 
